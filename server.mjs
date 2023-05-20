0fb524b9e4cc8588c4860ebc268fb34e4b365cce
errinjohnson <sporttrac64@outlook.com>
2023-05-19 22:24:42 -0500

    changing over to a module es6

--- server.mjs
+++ server.mjs
@@ -3,10 +3,9 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 import cors from 'cors';
 import path from 'path';
 
-
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'public'), {
+app.use(express.static(path.join(process.cwd(), 'public'), {
     setHeaders: (res, filePath) => {
         if (filePath.endsWith('.js')) {
             res.setHeader('Content-Type', 'application/javascript');
@@ -19,57 +18,40 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 // Serve the index.html file
-app.get('/public/index.html', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
 });
 
 app.use('/', createProxyMiddleware({
+    target: 'http://localhost:3000', // default target
     changeOrigin: true,
     logLevel: 'debug',
     onProxyReq(proxyReq, req, res) {
         proxyReq.setHeader('Referer', req.url);
     },
-    onProxyRes(proxyRes, req, res) {
-        // set cors headers in the response from the target server
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-        res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-        const setCookie = proxyRes.headers['set-cookie'];
-        if (setCookie) {
-            const cookies = Array.isArray(setCookie) ? setCookie : [setCookie];
-            cookies.forEach((cookie) => {
-                res.setHeader('Set-Cookie', cookie + '; domain=digitalocean.com; HttpOnly; Secure; SameSite=None');
-            });
-        }
-    },
     router: (req) => {
         const targetUrl = req.query.url;
-        if (!targetUrl) {
-            return null;
-        }
-        return targetUrl;
+        return targetUrl || null;
     },
- onError: (err, req, res) => {
-  if (err && err.status) {
-    console.error('Error in proxy middleware:', err);
-    res.status(err.status).send('Internal Server Error');
-  } else if (err) {
-    console.error('Unknown error in proxy middleware:', err);
-    res.status(500).send('Unknown Error');
-  } else {
-    res.status(500).send('Unknown Error');
-  }
-},
+    onError: (err, req, res) => {
+        if (err && err.status) {
+            console.error('Error in proxy middleware:', err);
+            res.status(err.status).send('Internal Server Error');
+        } else if (err) {
+            console.error('Unknown error in proxy middleware:', err);
+            res.status(500).send('Unknown Error');
+        } else {
+            res.status(500).send('Unknown Error');
+        }
+    },
     pathRewrite: (path, req) => {
-  const targetUrl = req.query.url;
-  if (!targetUrl) {
-    return path;
-  }
-  const url = new URL(targetUrl);
-  return url.pathname;
-},
-
+        const targetUrl = req.query.url;
+        if (!targetUrl) {
+            return path;
+        }
+        const url = new URL(targetUrl);
+        return url.pathname + url.search + url.hash;
+    },
 }));
 
 app.listen(port, () => {
